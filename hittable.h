f79e0170cd4e0828ebdc00b03de02f0b1b2b83c7
wanfeng-win <804121985@qq.com>
2025-11-12 15:01:26 +0800

    新增法线判断，hittableList

--- hittable.h
+++ hittable.h
@@ -8,6 +8,15 @@ class hit_record {
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+    // 用于区分外法线还是内法线，因为光线可能从球体内部击中球得表面，这时候的法线应该用指向圆心的法线（取反）
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 // 表示可击中
 class hittable {
