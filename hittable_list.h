2d64891ca6f1d6068f623024e6dc8ade23430e82
wanfeng-win <804121985@qq.com>
2025-11-13 11:49:49 +0800

    封装相机类和场景类

--- hittable_list.h
+++ hittable_list.h
@@ -3,12 +3,9 @@
 
 #include "hittable.h"
 
-#include <memory>
 #include <vector>
 
-using std::make_shared;
-using std::shared_ptr;
-
+// 其实就是一个场景类，包含所有可以被击中的物体，目前需要用for循环遍历每个物体，判断是否被击中
 class hittable_list : public hittable {
   public:
     std::vector<shared_ptr<hittable>> objects;
@@ -23,13 +20,13 @@ class hittable_list : public hittable {
     }
 
     // 遍历所有Mesh，返回最近的t
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         hit_record temp_rec;
         bool hit_anything = false;
-        auto closest_so_far = ray_tmax;
+        auto closest_so_far = ray_t.max;
 
         for (const auto& object : objects) {
-            if (object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
+            if (object->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
                 hit_anything = true;
                 closest_so_far = temp_rec.t;
                 rec = temp_rec;
--- main.cpp
+++ main.cpp
@@ -1,107 +1,20 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
+#include "camera.h"
 
-#include <iostream>
-
-// 判断r是否与这个球体相交 bool
-bool is_hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return discriminant>0;  
-}
-
-// 判断并计算交点，返回近的一个交点（不考虑t小于0）
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-     if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-b - std::sqrt(discriminant) ) / (2.0*a);
-    }
-}
-
-// 交点计算可以化简
-double hit_sphere_simple(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = r.direction().length_squared();
-    auto h = dot(r.direction(), oc);
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = h*h - a*c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (h - std::sqrt(discriminant)) / a;
-    }
-}
-
-color ray_color(const ray& r) {
-    auto t = hit_sphere_simple(point3(0,0,-1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));  // 法线方向，从圆心指向交点
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
-    }
-    
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
-}
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 int main() {
+    hittable_list world;
 
-    // Image
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
+    camera cam;
 
-    // Calculate the image height, and ensure that it's at least 1.
-    int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width  = 400;
 
-    // Camera
-    // 焦距 摄像机距离近平面距离
-    auto focal_length = 1.0; 
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/image_height);
-    auto camera_center = point3(0, 0, 0);
-
-    // 宽是X正方向
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    // 高是从上往下，Y负方向
-    auto viewport_v = vec3(0, -viewport_height, 0);
-
-    // 像素宽度
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // 计算左上角第一个像素位置。坐标系是根据摄像机位置创建的，所以像素位置也是根据摄像机位置来计算的
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-    // 移动到像素中心
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Render
-
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);  // 每个像素位置
-            auto ray_direction = pixel_center - camera_center; // 射线从摄像机指向像素点
-            ray r(camera_center, ray_direction); // Ray
-
-            color pixel_color = ray_color(r); // 计算Ray击中的颜色
-            write_color(std::cout, pixel_color); // 写入
-        }
-    }
-
-    std::clog << "\rDone.                 \n";
+    cam.render(world);
 }
